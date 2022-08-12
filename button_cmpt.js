b2fbf9962a93f95bdaa1b1aa3faa0e7556aabdae
Ely2112 <84430363+Ely2112@users.noreply.github.com>
2022-08-12 13:28:50 +0800

    234f

--- button_cmpt.js
+++ button_cmpt.js
@@ -1,7 +1,7 @@
 document.getElementById("KW_button").addEventListener("click", show_manga_KW);
 function show_manga_KW(){
   document.getElementById("KW_saying").style.opacity = "1" ;
-  for (i=1; i<10; i++){
+  for (i=1; i<11; i++){
     document.getElementById(`KW${i}`).style.opacity = "1" ;
     document.getElementById(`KW${i}`).style.height = "auto" ;
   }
